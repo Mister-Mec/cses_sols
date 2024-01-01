ed3619ad79eb457f388af7056876de609bff7c69
Mister-Mec <ndwals1999@gmail.com>
2024-01-01 13:36:51 +0530

    first commit

---
+++ csesGraph1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t; 
+    cin>>t; 
+    while(t--){
+        int n;
+        cin>>n; 
+        string s; 
+        cin>>s;
+        set<char> s1; 
+        for(int i = 0; i < n; i++) s1.insert(s[i]); 
+        int ind = s1.size();
+        int l = 1; 
+        long long ans = 0;
+        for(auto it = s1.begin(); it != s1.end(); it++){
+            cout << *it << endl;
+            cout << "11" << endl;
+            int t = s.find(*it); 
+            ans += (n-t);
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
---
+++ dda.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        // solve();
+        cout << "eneter number  1 : ";
+        int x ;
+        cin >> x ;
+        cout << x;
+    }
+    return 0;
+}
